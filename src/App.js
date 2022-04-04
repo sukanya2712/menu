c55148cb2fbe9fe0d7491a0898072c7563a1874d
sukanya2712 <sukanyanaik511@gmail.com>
2022-04-04 10:10:10 +0530

    menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import items from "./data";
+import Categories from "./Categories";
+import Menu from "./Menu"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const allCategories = ["All","Breakfast","Lunch","Shakes"]
+const allCat = items.map(item=>item.category)
+const tempSet = new Set(allCat)
+let tempArr = [...tempSet]
+
+// 1st way
+const finalArr = ["all",...tempArr]
+
+//2nd way
+tempArr.unshift("all")
+console.log(tempArr,finalArr)
+
+const allCategories = ["all",...new Set(items.map((item)=>item.category))]
+
+const App = () => {
+
+    const [menuItems,setMenuItems] = useState(items)
+    const [categories,setCategories] = useState(allCategories)
+
+
+    const filterItems = (categoryName) => {
+        if(categoryName === "all"){
+            setMenuItems(items)
+        }else{
+            const newItems = items.filter((item)=>item.category===categoryName)
+            setMenuItems(newItems)
+        }
+    }
+    return(
+        <main>
+            <section className="menu section">
+                <div className="title">
+                    <h2>Our Menu</h2>
+                    <div className="underline"></div>    
+                </div>
+                <Categories categories={categories} filterItems={filterItems}/>
+                <Menu items = {menuItems} />
+            </section>
+        </main>
+    )
+
 }
 
 export default App;
